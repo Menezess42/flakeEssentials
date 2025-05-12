e3f9c2da35148726fab86476442310141d2783e8
Menezess42 <arielmenezess@hotmail.com>
2025-05-12 11:08:04 -0300

    Flake Essentials

---
+++ Python/default.nix
@@ -0,0 +1,26 @@
+{ pkgs }:
+
+pkgs.mkShell {
+  name = "essentials-python-env";
+  buildInputs = with pkgs; [
+                python311
+                python311Packages.pip
+                # IDE Like Features
+                pyright
+                python311Packages.pip
+                python311Packages.jedi
+                python311Packages.jedi-language-server
+                python311Packages.black
+                python311Packages.flake8
+                python311Packages.sentinel
+                python311Packages.python-lsp-server
+                python311Packages.virtualenv
+                python311Packages.pyflakes  # Linter Pyflakes
+                python311Packages.isort
+                # Libs for the book
+                python311Packages.matplotlib
+  ];
+  shellHook = ''
+    echo "Ambiente Python Essencial carregado!"
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Ambientes essenciais de desenvolvimento do Sr. Menezes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonShell = import ./Python/default.nix { inherit pkgs; };
+        # goShell = import ./Go/default.nix { inherit pkgs; };
+        # jsShell = import ./JS/default.nix { inherit pkgs; };
+      in
+      {
+        devShells = {
+          python = pythonShell;
+          # go = goShell;
+          # js = jsShell;
+        };
+      });
+}
