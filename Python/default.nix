139a7eb808413d8ff222849fc9be00c4ef81fcbd
Menezess42 <arielmenezess@hotmail.com>
2025-05-27 20:01:46 -0300

    Try to deactivate pycheck

--- Python/default.nix
+++ Python/default.nix
@@ -1,27 +1,42 @@
-{ pkgs }:
+{ pkgs ? import <nixpkgs> {
+    overlays = [
+      (final: prev: {
+        python311 = prev.python311.override {
+          packageOverrides = pyself: pysuper: {
+            buildPythonPackage = args: pysuper.buildPythonPackage (args // {
+              doCheck = false;
+              checkPhase = "echo 'checkPhase desativado para buildPythonPackage.'";
+            });
+          };
+        };
+      })
+    ];
+  }
+}:
 
 pkgs.mkShell {
   name = "essentials-python-env";
+
   buildInputs = with pkgs; [
-                python311
-                python311Packages.pip
-                # IDE Like Features
-                pyright
-                python311Packages.pip
-                python311Packages.jedi
-                python311Packages.jedi-language-server
-                python311Packages.black
-                python311Packages.flake8
-                python311Packages.sentinel
-                python311Packages.python-lsp-server
-                python311Packages.virtualenv
-                python311Packages.pyflakes  # Linter Pyflakes
-                python311Packages.isort
-                # Libs for the book
-                python311Packages.matplotlib
-                python311Packages.seaborn
-                python311Packages.numpy
+    python311
+    python311Packages.pip
+    # IDE Like Features
+    pyright
+    python311Packages.jedi
+    python311Packages.jedi-language-server
+    python311Packages.black
+    python311Packages.flake8
+    python311Packages.sentinel
+    python311Packages.python-lsp-server
+    python311Packages.virtualenv
+    python311Packages.pyflakes
+    python311Packages.isort
+    # Libs for the book
+    python311Packages.matplotlib
+    python311Packages.seaborn
+    python311Packages.numpy
   ];
+
   shellHook = ''
     echo "Ambiente Python Essencial carregado!"
   '';
