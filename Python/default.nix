d0c0d61855efbd6bdcca0894036ceaca3716e686
Menezess42 <arielmenezess@hotmail.com>
2025-11-06 15:12:54 -0300

    test

--- Python/default.nix
+++ Python/default.nix
@@ -1,24 +1,60 @@
-{pkgs}:
-pkgs.mkShell {
-  name = "essentials-python-env";
-  config.doCheck=false;
-  buildInputs = with pkgs; [
-                python311
-                python311Packages.pip
-                # IDE Like Features
-                pyright
-                python313Packages.pip
-                python313Packages.jedi
-                python313Packages.jedi-language-server
-                python313Packages.black
-                python313Packages.flake8
-                python313Packages.sentinel
-                python313Packages.python-lsp-server
-                python313Packages.virtualenv
-                python313Packages.pyflakes  # Linter Pyflakes
-                python313Packages.isort
-  ];
-  shellHook = ''
-    echo "Ambiente Python Essencial carregado!"
-  '';
+{ pkgs }:
+let
+  python = pkgs.python313;
+
+  # módulo que será reutilizável por outros flakes
+  pythonModule = {
+    buildInputs = with pkgs; [
+      python
+      python.pythonPackages.pip
+      pkgs.pyright
+      python.pythonPackages.jedi
+      python.pythonPackages.black
+      # ... outras libs comuns
+    ];
+
+    shellHook = ''
+      echo "Ambiente Python Essencial carregado!"
+    '';
+
+    env = {
+      # se precisar variáveis compartilhadas
+      PYTHONNOUSERSITE = "1";
+    };
+  };
+
+  # shell pronto (compatibilidade backward)
+  pythonShell = pkgs.mkShell {
+    name = "essentials-python-env";
+    buildInputs = pythonModule.buildInputs;
+    shellHook = pythonModule.shellHook;
+  };
+in
+{
+  module = pythonModule;
+  shell  = pythonShell;
 }
+# {pkgs}:
+# pkgs.mkShell {
+#   name = "essentials-python-env";
+#   config.doCheck=false;
+#   buildInputs = with pkgs; [
+#                 python311
+#                 python311Packages.pip
+#                 # IDE Like Features
+#                 pyright
+#                 python313Packages.pip
+#                 python313Packages.jedi
+#                 python313Packages.jedi-language-server
+#                 python313Packages.black
+#                 python313Packages.flake8
+#                 python313Packages.sentinel
+#                 python313Packages.python-lsp-server
+#                 python313Packages.virtualenv
+#                 python313Packages.pyflakes  # Linter Pyflakes
+#                 python313Packages.isort
+#   ];
+#   shellHook = ''
+#     echo "Ambiente Python Essencial carregado!"
+#   '';
+# }
--- flake.nix
+++ flake.nix
@@ -5,21 +5,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system;  config.doCheck = false;};
-        pythonShell = import ./Python/default.nix { inherit pkgs; };
-        # goShell = import ./Go/default.nix { inherit pkgs; };
-        jsShell = import ./Js/default.nix {inherit pkgs; };
-        pythonMLShell = import ./PythonML/default.nix {inherit pkgs; };
-      in
-      {
-        devShells = {
-          python = pythonShell;
-          js  = jsShell;
-          pythonML = pythonMLShell;
-        };
-      });
+    # flake.nix (essentials) — trecho relevante
+    outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; config.doCheck = false; };
+
+          pythonShell = import ./Python/default.nix { inherit pkgs; };
+          pythonMLShell = import ./PythonML/default.nix { inherit pkgs; };
+          jsShell = import ./Js/default.nix { inherit pkgs; };
+
+        in {
+          # Devshells para uso rápido (compatibilidade)
+          devShells = {
+            python    = pythonShell.shell;     # mkShell pronto
+            pythonML  = pythonMLShell.shell;   # mkShell pronto (opcional)
+            js        = jsShell.shell;
+          };
+
+          # Modules exportáveis para composição
+          modules = {
+            python = {
+              base = pythonShell.module;      # { buildInputs, shellHook, env, ... }
+            };
+            ml = {
+              python = pythonMLShell.module;  # { buildInputs, shellHook, env, ... }
+            };
+            js = { base = jsShell.module; };
+          };
+        });
+  # outputs = { self, nixpkgs, flake-utils }:
+  #   flake-utils.lib.eachDefaultSystem (system:
+  #     let
+  #       pkgs = import nixpkgs { inherit system;  config.doCheck = false;};
+  #       pythonShell = import ./Python/default.nix { inherit pkgs; };
+  #       # goShell = import ./Go/default.nix { inherit pkgs; };
+  #       jsShell = import ./Js/default.nix {inherit pkgs; };
+  #       pythonMLShell = import ./PythonML/default.nix {inherit pkgs; };
+  #     in
+  #     {
+  #       devShells = {
+  #         python = pythonShell;
+  #         js  = jsShell;
+  #         pythonML = pythonMLShell;
+  #       };
+  #     });
 }
 
