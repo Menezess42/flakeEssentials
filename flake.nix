763790360527e99cf9b4c10449f9bdef950b738f
Menezess42 <arielmenezess@hotmail.com>
2025-11-06 15:55:20 -0300

    test

--- flake.nix
+++ flake.nix
@@ -34,21 +34,5 @@
             js = { base = jsShell.module; };
           };
         });
-  # outputs = { self, nixpkgs, flake-utils }:
-  #   flake-utils.lib.eachDefaultSystem (system:
-  #     let
-  #       pkgs = import nixpkgs { inherit system;  config.doCheck = false;};
-  #       pythonShell = import ./Python/default.nix { inherit pkgs; };
-  #       # goShell = import ./Go/default.nix { inherit pkgs; };
-  #       jsShell = import ./Js/default.nix {inherit pkgs; };
-  #       pythonMLShell = import ./PythonML/default.nix {inherit pkgs; };
-  #     in
-  #     {
-  #       devShells = {
-  #         python = pythonShell;
-  #         js  = jsShell;
-  #         pythonML = pythonMLShell;
-  #       };
-  #     });
 }
 
